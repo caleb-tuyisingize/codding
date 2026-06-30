960d131b6b846d394adf15843e8e071e6cdaaed7
Caleb TUYISINGIZE <calebmevis11@gmail.com>
2026-06-30 12:58:01 +0200

    .

---
+++ server.js
@@ -0,0 +1,71 @@
+import fs from "fs";
+import express from "express";
+import data from "./files/data.json" with { type: "json" };
+const app = express();
+app.use(express.json());
+app.get("/all-data", (req, res) => {
+  res.json(data);
+});
+
+app.post("/enter-data", (req, res) => {
+  const { name, age, type } = req.body;
+  const id = data.length - 1;
+  const autoId = data[id];
+  const userData = { id: autoId.id + 1, name, age, type };
+  data.push(userData);
+  try {
+    fs.writeFile("./files/data.json", JSON.stringify(data), (err) => {
+      if (err) {
+        res.status(404).json({ message: "Failed to write file" });
+      }
+    });
+    res.status(200).send(userData);
+  } catch (error) {
+    res.status(500).json({ message: "Server error occured" });
+  }
+});
+
+app.put("/change-data/:id", (req, res) => {
+  const id = Number(req.params.id);
+  const { name, age, type } = req.body;
+  const updatedData = { name, age, type };
+  const newDataUpdated = data.find((a) => a.id === id);
+
+  if (!newDataUpdated) {
+    res.status(404).send("ID not found");
+  }
+
+  newDataUpdated.name = name;
+  newDataUpdated.age = age;
+  newDataUpdated.type = type;
+  try {
+    fs.writeFile("./files/data.json", JSON.stringify(data), (err) => {
+      res.status(404).json({ message: "Failed to update" });
+    });
+    res.send(updatedData);
+  } catch (error) {
+    res.status(500).json({ message: "Server error occured" });
+  }
+});
+
+app.delete("/remove-data/:id", (req, res) => {
+  const id = Number(req.params.id);
+  const initialLength = data.length;
+  const deletedData = data.filter((user) => user.id !== id);
+  if (initialLength === deletedData.length) {
+    res.status(404).json({ message: "user with this id not found" });
+  }
+  try {
+    fs.writeFile("./files/data.json", JSON.stringify(deletedData), (error) => {
+      if (error) {
+        res.status(401).json({ message: "Bad request" });
+      }
+    });
+    res.status(200).json({ message: "User deleted successfully" });
+  } catch (error) {
+    res.status(500).json({ message: "Server error occured" });
+  }
+});
+
+app.listen(3000);
+console.log("Server is running....");
